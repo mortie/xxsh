c2fd08c893e4b189c8f96ad0d9cd33c41f0928bf
Martin Dørum <martid0311@gmail.com>
2021-05-05 23:26:35 +0200

    support redirections, use linenoise for readline

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 VERSION := 0.1
 XXSH_VERSION := "\"$(VERSION) $(shell git rev-parse --short HEAD)\""
 
-CFLAGS += -Wall -Wextra -Wno-unused-parameter -Wpedantic -DXXSH_VERSION=$(XXSH_VERSION)
-xxsh: xxsh.c
+CFLAGS += -Ilinenoise -Wall -Wextra -Wno-unused-parameter -Wpedantic -DXXSH_VERSION=$(XXSH_VERSION)
+xxsh: xxsh.c linenoise/linenoise.c
 
 .PHONY: clean
 clean:
--- xxsh.c
+++ xxsh.c
@@ -8,12 +8,17 @@
 #include <grp.h>
 #include <pwd.h>
 #include <errno.h>
+#include <signal.h>
+
+#include "linenoise/linenoise.h"
 
 #ifndef XXSH_VERSION
 #define XXSH_VERSION "version unknown"
 #endif
 
+// TODO: rm, rmdir, mkdir, execute file
 #define commands \
+	X(echo, "Echo text") \
 	X(ls, "List files in a directory") \
 	X(stat, "Get info about a file") \
 	X(pwd, "Print the current working directory") \
@@ -23,6 +28,16 @@
 	X(exit, "Exit XXSH")
 
 static int running = 1;
+static FILE *outf;
+
+static void readstr(char *line, size_t *start, size_t *end) {
+	char *s = line;
+	while (*line == ' ' || *line == '\t') line += 1;
+	*start = line - s;
+
+	while (*line != ' ' && *line != '\t' && *line != '\n' && *line != '\0') line += 1;
+	*end = line - s;
+}
 
 static int readarg(char **line, char **arg) {
 	if (**line == '\0') {
@@ -30,16 +45,32 @@ static int readarg(char **line, char **arg) {
 		return 0;
 	}
 
-	while (**line == ' ' || **line == '\t') *line += 1;
-	*arg = *line;
-
-	while (**line != ' ' && **line != '\t' && **line != '\n' && **line != '\0') *line += 1;
+	size_t start, end;
+	readstr(*line, &start, &end);
+	*arg = *line + start;
+	*line += end;
 	if (**line != '\0') {
 		**line = '\0';
 		*line += 1;
 	}
 
 	return **arg != '\0';
+}
+
+static int do_echo(char **line) {
+	char *arg;
+	int first = 1;
+	while (readarg(line, &arg)) {
+		if (first) {
+			fprintf(outf, "%s", arg);
+			first = 0;
+		} else {
+			fprintf(outf, " %s", arg);
+		}
+	}
+
+	putc('\n', outf);
+	return 0;
 }
 
 static int ls_path(char *path) {
@@ -50,9 +81,22 @@ static int ls_path(char *path) {
 		return -1;
 	}
 
+	size_t linelen = 0;
 	for (int i = 0; i < n; ++i) {
-		printf("%s\n", names[i]->d_name);
+		size_t namelen = strlen(names[i]->d_name);
+		if (linelen != 0 && linelen + namelen > 80) {
+			fprintf(outf, "\n");
+			linelen = 0;
+		}
+
+		if (linelen == 0) {
+			fprintf(outf, "%s", names[i]->d_name);
+		} else {
+			fprintf(outf, "  %s", names[i]->d_name);
+		}
+		linelen += namelen + 2;
 	}
+	fprintf(outf, "\n");
 
 	free(names);
 	return 0;
@@ -71,14 +115,14 @@ static int do_ls(char **line) {
 		return ls_path(arg);
 	}
 
-	printf("%s:\n", arg);
+	fprintf(outf, "%s:\n", arg);
 	int ret;
 	if ((ret = ls_path(arg)) < 0) {
 		return ret;
 	}
 
 	do {
-		printf("%s\n", arg2);
+		fprintf(outf, "%s\n", arg2);
 		if ((ret = ls_path(arg2)) < 0) {
 			return ret;
 		}
@@ -114,7 +158,7 @@ static int do_stat(char **line) {
 			strncpy(gname, gr->gr_name, sizeof(gname));
 		}
 
-		printf("%s: %o: %s:%s\n", arg, st.st_mode, uname, gname);
+		fprintf(outf, "%s: %o: %s:%s\n", arg, st.st_mode, uname, gname);
 	}
 
 	return 0;
@@ -125,11 +169,11 @@ static int do_pwd(char **line) {
 	errno = 0;
 	if (getcwd(path, sizeof(path)) == NULL) {
 		perror("getcwd");
-		return 0;
+		return -1;
 	}
 
-	printf("%s\n", path);
-	return -1;
+	fprintf(outf, "%s\n", path);
+	return 0;
 }
 
 static int do_cat(char **line) {
@@ -154,10 +198,10 @@ static int do_cat(char **line) {
 				break;
 			}
 
-			fwrite(buf, 1, n, stdout);
+			fwrite(buf, 1, n, outf);
 		}
 
-		fflush(stdout);
+		fflush(outf);
 		if (fclose(f) < 0) {
 			perror("fclose");
 		}
@@ -184,7 +228,7 @@ static int do_cd(char **line) {
 }
 
 static int do_help(char **line) {
-#define X(name, desc) printf("%s: %s\n", #name, desc);
+#define X(name, desc) fprintf(outf, "%s: %s\n", #name, desc);
 	commands
 #undef X
 
@@ -197,38 +241,109 @@ static int do_exit(char **line) {
 }
 
 static int run(char *line) {
-	char *command;
-	if (!readarg(&line, &command)) {
-		return 0;
+	char redirpath[1024];
+	int redirect = 0;
+	int retval = 0;
+
+	FILE *redirf = NULL;
+	FILE *oldoutf = outf;
+
+	size_t linelen = strlen(line);
+	for (size_t i = 0; i < linelen; ++i) {
+		if (line[i] == '>') {
+			size_t start = 0;
+			size_t end = 0;
+			readstr(line + i + 1, &start, &end);
+			start += i + 1;
+			end += i + 1;
+
+			if (line[start] == '\0') {
+				fprintf(stderr, "Redirect with no argument\n");
+				retval = -1;
+				goto exit;
+			}
+
+			if (end - start >= sizeof(redirpath) - 1) {
+				fprintf(stderr, "Too long redirection argument!\n");
+				retval = -1;
+				goto exit;
+			}
+
+			memcpy(redirpath, line + start, end - start);
+			redirpath[end - start] = '\0';
+
+			size_t removefrom = i;
+			size_t removeto = end;
+
+			memmove(line + removefrom, line + removeto + 1, linelen - removeto + 1);
+			redirect = 1;
+			break;
+		}
 	}
 
-#define X(name, desc) if (strcmp(command, #name) == 0) return do_ ## name(&line);
+	if (redirect) {
+		redirf = fopen(redirpath, "w");
+		if (redirf == NULL) {
+			perror(redirpath);
+			retval = -1;
+			goto exit;
+		}
+
+		fflush(outf);
+		outf = redirf;
+	}
+
+	char *command;
+	if (!readarg(&line, &command)) {
+		goto exit;
+	}
+
+#define X(name, desc) \
+		if (strcmp(command, #name) == 0) { \
+			retval = do_ ## name(&line); \
+			goto exit; \
+		}
 	commands
 #undef X
 
 	fprintf(stderr, "Unknown command '%s'\n", command);
-	return 1;
+	retval = -1;
+	goto exit;
+
+exit:
+	if (redirf != NULL) {
+		fclose(redirf);
+		outf = oldoutf;
+	}
+
+	return retval;
 }
 
 int main() {
-	printf("XXSH %s\n", XXSH_VERSION);
+	outf = stdout;
+
+	fprintf(outf, "XXSH %s\n", XXSH_VERSION);
+	linenoiseHistorySetMaxLen(64);
 	int ret = 0;
-	char line[4096];
 	while (running) {
-		printf(">> ");
-		if (fgets(line, sizeof(line), stdin) == NULL) {
-			perror("stdin");
-			ret = 1;
+		errno = 0;
+		char *line = linenoise(">> ");
+		if (line == NULL && errno == EAGAIN) { // CTRL + C
+			continue;
+		} else if (line == NULL) {
 			break;
 		}
 
+		linenoiseHistoryAdd(line);
+
 		int ret;
 		ret = run(line);
+		linenoiseFree(line);
 		if (ret < 0) {
 			fprintf(stderr, ":( %i\n", ret);
 		}
 	}
 
-	printf("Exit.\n");
+	fprintf(outf, "Exit.\n");
 	return ret;
 }
